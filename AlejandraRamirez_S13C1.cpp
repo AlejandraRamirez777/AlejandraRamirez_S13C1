9e8a51b2b5e3a7fc849578473f88f2fa3c7b8ee0
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-04-24 18:16:42 -0500

    Avance

--- AlejandraRamirez_S13C1.cpp
+++ AlejandraRamirez_S13C1.cpp
@@ -20,28 +20,48 @@ int main () {
     int nR= 100;
     int nC= 100;
     
+    //T presente
     vector<vector<double> > T(nR);
+    //T futuro
+    vector<vector<double> > Tf(nR);
 
-    //Generar matrix
+    //Generar matrices
     for (int i = 0; i < T.size(); i++) {
         T[i] = vector<double>(nC);
+	Tf[i] = vector<double>(nC);
     }
 
-    // Matrix: vector 2d llenado
+    // Matriz: vector 2d llenados
     for (int i = 0; i < T.size(); i++) {
         for (int k = 0; k < T[i].size(); k++){
             if((i >= 40 and i <= 60) and ( k >= 10 and k <= 30)){
-                T[i][k] = 100;
-		cout << T[i][k] << " ";
+                 T[i][k] = 100;
+		 Tf[i][k] = 100;
+	        //cout << T[i][k] << " ";
             }
             else{
                 T[i][k] = 50;
-                cout << T[i][k] << " ";
+                Tf[i][k] = 50;
+                //cout << T[i][k] << " ";
             }
         }
-        cout << endl;
+        //cout << endl;
     }
 
+    double t = 0;
+    //Generar recorrido
+    while(t <2500){
+        for(int i=0; i<T.size();i++){
+            for(int k=1; T[i].size();k++){
+                //Tf[i][k] = 
+                //Uf[k] = U[k] - c*(dt/dx)*(U[k]-U[k-1]);
+	        //cout << X[k] <<  " " << Uf[k] << endl;
+            }
+            ///copy(Uf.begin(), Uf.end(), U.begin());
+        }
+
+     t += dt;
+    }
 
     
 return 0;
